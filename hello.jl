633222fef1fc70849b314c9a1c58faa1e0873f64
atroopa <atroopa@gmail.com>
2022-04-16 14:47:32 +0430

    i learn Const in Julia

--- hello.jl
+++ hello.jl
@@ -1,5 +1,16 @@
 
 
-my_last_name = "Omid"
+const my_last_name = "Hajavi"
 
 println(my_last_name)
+
+my_last_name = "Arbabi"
+
+println(my_last_name)
+
+
+const my_age = 29
+
+my_age = 30 # Not Float Not String
+
+println(my_age)
