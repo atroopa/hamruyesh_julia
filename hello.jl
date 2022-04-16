d155abeacaaefcaca63d03355961c2a7bc01dcdc
atroopa <atroopa@gmail.com>
2022-04-16 15:09:36 +0430

    i learn string in julia

--- hello.jl
+++ hello.jl
@@ -14,3 +14,11 @@ const my_age = 29
 my_age = 30 # Not Float Not String
 
 println(my_age)
+
+println(repeat("--", 20))
+
+my_first_string = "Hello"
+
+println("First Char is : ",my_first_string[1])
+println("length is     : " , length(my_first_string))
+
